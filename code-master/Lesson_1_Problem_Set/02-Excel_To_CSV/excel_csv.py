1848b14ea65b40dcea09adfa70c296db332f57e8
qkitolindo <josemanu@gmail.com>
2017-03-17 10:26:39 -0400

    Update:         ../../../.ipynb_checkpoints/Exercises-checkpoint.ipynb         ../../../Exercises.ipynb         excel_csv.py         output.csv

--- code-master/Lesson_1_Problem_Set/02-Excel_To_CSV/excel_csv.py
+++ code-master/Lesson_1_Problem_Set/02-Excel_To_CSV/excel_csv.py
@@ -3,10 +3,14 @@
 # COAST, EAST, FAR_WEST, NORTH, NORTH_C, SOUTHERN, SOUTH_C, WEST
 # and write the result out in a csv file, using pipe character | as the delimiter.
 # An example output can be seen in the "example.csv" file.
+import xlwt
 import xlrd
 import os
 import csv
 from zipfile import ZipFile
+
+os.chdir("/Users/josemanuelfernandez/Documents/Udacity/Data_Analyst/P3/Data-Wrangling/Lesson-1/code-master/Lesson_1_Problem_Set/02-Excel_To_CSV/")
+
 datafile = "2013_ERCOT_Hourly_Load_Data.xls"
 outfile = "2013_Max_Loads.csv"
 
@@ -19,23 +23,57 @@ def open_zip(datafile):
 def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
-    data = None
+    data = []
+    #data_dict = {}
+
+    for col in range(1, (sheet.ncols - 1)): # Only considers col 1-9 (stations)
+        cv = sheet.col_values(col, start_rowx=1, end_rowx=None)
+        maxload = max(cv)
+        maxpos = cv.index(maxload) + 1 # Obs. start at row=1 (index starts at row =0)
+                                  # so, position is +1
+        maxtime = sheet.cell_value(maxpos, 0) # "Value in row-cell = 'maxpos'
+                                          # and col 0 for column with time-data:"
+        realmaxtime = xlrd.xldate_as_tuple(maxtime, 0) # Converts 'maxtime' into a tuple with time format
+        station = sheet.cell_value(0,col)
+        data_dict = {
+            'Station': station,
+            'Year': realmaxtime[0],
+            'Month': realmaxtime[1],
+            'Day': realmaxtime[2],
+            'Hour': realmaxtime[3],
+            'Max Load': maxload}
+        data.append(data_dict)
     # YOUR CODE HERE
     # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
     # Excel date to Python tuple of (year, month, day, hour, minute, second)
-    return data
+    return (data)
+data = (parse_file(datafile))
+
+#print (data)
+
+
 
 def save_file(data, filename):
-    # YOUR CODE HERE
+    filename = ''.join((filename, '.csv'))
+    #header = ['Station', 'Year', 'Month', 'Day', 'Hour', 'Max Load']
 
-    
+    with open(filename,'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys(),
+                            delimiter='|')
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+save_file(data, "output")
+
+'''
 def test():
     open_zip(datafile)
     data = parse_file(datafile)
     save_file(data, outfile)
 
     ans = {'FAR_WEST': {'Max Load': "2281.2722140000024", 'Year': "2013", "Month": "6", "Day": "26", "Hour": "17"}}
-    
+
     fields = ["Year", "Month", "Day", "Hour", "Max Load"]
     with open(outfile) as of:
         csvfile = csv.DictReader(of, delimiter="|")
@@ -45,5 +83,6 @@ def test():
                 for field in fields:
                     assert ans[s][field] == line[field]
 
-        
+
 test()
+'''
