4b6cfdead2203c65736ffae87a6e34b21e263d36
qkitolindo <josemanu@gmail.com>
2017-03-20 22:24:38 -0400

    Add and Update:
                    modified:   .ipynb_checkpoints/Exercises-checkpoint.ipynb
                    modified:   Exercises.ipynb
                    modified:   code-master/Lesson_1_Problem_Set/03-Wrangling_JSON/nytimes.py

--- code-master/Lesson_1_Problem_Set/03-Wrangling_JSON/nytimes.py
+++ code-master/Lesson_1_Problem_Set/03-Wrangling_JSON/nytimes.py
@@ -6,41 +6,84 @@ This exercise shows some important concepts that you should be aware about:
 - using authentication with web APIs
 - using offset when accessing web APIs
 
-To run this code locally you have to register at the NYTimes developer site 
-and get your own API key. You will be able to complete this exercise in our UI without doing so,
-as we have provided a sample result.
+To run this code locally you have to register at the NYTimes developer site
+and get your own API key. You will be able to complete this exercise in our UI
+without doing so, as we have provided a sample result. (See the file
+'popular-viewed-1.json' from the tabs above.)
 
-Your task is to process the saved file that represents the most popular (by view count)
-articles in the last day, and return the following data:
-- list of dictionaries, where the dictionary key is "section" and value is "title"
-- list of URLs for all media entries with "format": "Standard Thumbnail"
+Your task is to modify the article_overview() function to process the saved
+file that represents the most popular articles (by view count) from the last
+day, and return a tuple of variables containing the following data:
+- labels: list of dictionaries, where the keys are the "section" values and
+  values are the "title" values for each of the retrieved articles.
+- urls: list of URLs for all 'media' entries with "format": "Standard Thumbnail"
 
-All your changes should be in the article_overview function.
-The rest of functions are provided for your convenience, if you want to access the API by yourself.
+All your changes should be in the article_overview() function. See the test()
+function for examples of the elements of the output lists.
+The rest of functions are provided for your convenience, if you want to access
+the API by yourself.
 """
 import json
 import codecs
 import requests
 
+
 URL_MAIN = "http://api.nytimes.com/svc/"
 URL_POPULAR = URL_MAIN + "mostpopular/v2/"
-API_KEY = { "popular": "",
-            "article": ""}
+API_KEY = { "popular": "68580a55ee9d40ecaceb790ab2a6d573",
+            "article": "68580a55ee9d40ecaceb790ab2a6d573"}
 
 
 def get_from_file(kind, period):
     filename = "popular-{0}-{1}.json".format(kind, period)
-    with open(filename, "r") as f:
+    print (filename)
+    with open(filename, "r", newline='') as f:
         return json.loads(f.read())
+
+#data = get_from_file("viewed", 1)
+
+'''
+labels = dict()
+titles = list()
+#urls = list()
+#labels = list()
+#print(data)
+#labels = {}
+for article in data:
+    labels[article["section"]] = article["title"]
+    print (labels)
+    #titles.append(labels)
+    #labels[(article["section"])] : article["title"])
+#labels = dict((article["section"], article["title"]) for article in data)
+#print (labels)
+#for section, titles in labels.items():
+    #title.append(titles)
+#sections = list(article["section"] for article in data)
+#titles = list(article["section"] for article in data)
+#urls = list(article["url"] for article in data)
+#print (len(titles))
+#print ((titles[0]))
+'''
 
 
 def article_overview(kind, period):
     data = get_from_file(kind, period)
-    titles = []
-    urls =[]
-    # YOUR CODE HERE
+    titles = list()
+    urls = list()
+    for article in data:
+        section = article["section"]
+        title = article["title"]
+        titles.append({section: title})
+    #labels = [[article["section"],article["title"]] for article in data]
+        if "media" in article:
+                for m in article["media"]:
+                    for mm in m["media-metadata"]:
+                        if mm["format"] == "Standard Thumbnail":
+                            urls.append(mm["url"])
 
     return (titles, urls)
+
+titles, urls = article_overview("viewed", 1)
 
 
 def query_site(url, target, offset):
@@ -49,8 +92,8 @@ def query_site(url, target, offset):
     # NYTimes returns 20 articles per request, if you want the next 20
     # You have to provide the offset parameter
     if API_KEY["popular"] == "" or API_KEY["article"] == "":
-        print "You need to register for NYTimes Developer account to run this program."
-        print "See Intructor notes for information"
+        #print "You need to register for NYTimes Developer account to run this program."
+        #print "See Intructor notes for information"
         return False
     params = {"api-key": API_KEY[target], "offset": offset}
     r = requests.get(url, params = params)
@@ -65,13 +108,13 @@ def get_popular(url, kind, days, section="all-sections", offset=0):
     # This function will construct the query according to the requirements of the site
     # and return the data, or print an error message if called incorrectly
     if days not in [1,7,30]:
-        print "Time period can be 1,7, 30 days only"
+        print ("Time period can be 1,7, 30 days only")
         return False
     if kind not in ["viewed", "shared", "emailed"]:
-        print "kind can be only one of viewed/shared/emailed"
+        print ("kind can be only one of viewed/shared/emailed")
         return False
 
-    url = URL_POPULAR + "most{0}/{1}/{2}.json".format(kind, section, days)
+    url += "most{0}/{1}/{2}.json".format(kind, section, days)
     data = query_site(url, "popular", offset)
 
     return data
@@ -83,13 +126,12 @@ def save_file(kind, period):
     data = get_popular(URL_POPULAR, "viewed", 1)
     num_results = data["num_results"]
     full_data = []
-    with codecs.open("popular-{0}-{1}-full.json".format(kind, period), encoding='utf-8', mode='w') as v:
-        for offset in range(0, num_results, 20):        
+    with codecs.open("popular-{0}-{1}.json".format(kind, period), encoding='utf-8', mode='w') as v:
+        for offset in range(0, num_results, 20):
             data = get_popular(URL_POPULAR, kind, period, offset=offset)
             full_data += data["results"]
-        
-        v.write(json.dumps(full_data, indent=2))
 
+        v.write(json.dumps(full_data, indent=2))
 
 def test():
     titles, urls = article_overview("viewed", 1)
